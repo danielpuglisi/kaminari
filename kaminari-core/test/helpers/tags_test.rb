90f4b99ceff1e90479978f636d1e44936bc3e90f
AmandaBott <tgamandabot@gmail.com>
2018-05-13 12:06:00 -0300

    Amanda checking some code smells

--- kaminari-core/test/helpers/tags_test.rb
+++ kaminari-core/test/helpers/tags_test.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
+
 require 'test_helper'
 
 if defined?(::Rails::Railtie) && defined?(ActionView)
   class TagTest < ActionView::TestCase
     sub_test_case '#page_url_for' do
       setup do
-        self.params[:controller] = 'users'
-        self.params[:action]     = 'index'
+        params[:controller] = 'users'
+        params[:action]     = 'index'
       end
 
       sub_test_case 'for first page' do
@@ -26,9 +27,9 @@ if defined?(::Rails::Railtie) && defined?(ActionView)
 
       sub_test_case 'with a friendly route setting' do
         setup do
-          self.params[:controller] = 'addresses'
-          self.params[:action]     = 'index'
-          self.params[:page]       = 3
+          params[:controller] = 'addresses'
+          params[:action]     = 'index'
+          params[:page]       = 3
         end
 
         sub_test_case 'for first page' do
@@ -51,13 +52,13 @@ if defined?(::Rails::Railtie) && defined?(ActionView)
         end
       end
 
-      sub_test_case "with a custom route setting" do
+      sub_test_case 'with a custom route setting' do
         include CustomRoutesHelper
 
         setup do
-          self.params[:controller] = nil
-          self.params[:action] = nil
-          self.params[:page] = 3
+          params[:controller] = nil
+          params[:action] = nil
+          params[:page] = 3
         end
 
         sub_test_case 'for first page' do
@@ -82,7 +83,7 @@ if defined?(::Rails::Railtie) && defined?(ActionView)
 
       sub_test_case "with param_name = 'user[page]' option" do
         setup do
-          self.params[:user] = {page: '3', scope: 'active'}
+          params[:user] = { page: '3', scope: 'active' }
         end
 
         test 'for first page' do
@@ -91,14 +92,14 @@ if defined?(::Rails::Railtie) && defined?(ActionView)
         end
 
         test 'for other page' do
-          assert_match(/user%5Bpage%5D=2/, Kaminari::Helpers::Tag.new(self, param_name: 'user[page]').page_url_for(2))  # match user[page]=2
-          assert_match(/user%5Bscope%5D=active/, Kaminari::Helpers::Tag.new(self, param_name: 'user[page]').page_url_for(2))  # match user[scope]=active
+          assert_match(/user%5Bpage%5D=2/, Kaminari::Helpers::Tag.new(self, param_name: 'user[page]').page_url_for(2)) # match user[page]=2
+          assert_match(/user%5Bscope%5D=active/, Kaminari::Helpers::Tag.new(self, param_name: 'user[page]').page_url_for(2)) # match user[scope]=active
         end
       end
 
       sub_test_case "with param_name = 'foo.page' option" do
         setup do
-          self.params['foo.page'] = 2
+          params['foo.page'] = 2
         end
 
         test 'for first page' do
@@ -115,83 +116,83 @@ if defined?(::Rails::Railtie) && defined?(ActionView)
   class PaginatorTest < ActionView::TestCase
     test '#current?' do
       # current_page == page
-      assert_true Kaminari::Helpers::Paginator::PageProxy.new({current_page: 26}, 26, nil).current?
+      assert_true Kaminari::Helpers::Paginator::PageProxy.new({ current_page: 26 }, 26, nil).current?
       # current_page != page
-      assert_false Kaminari::Helpers::Paginator::PageProxy.new({current_page: 13}, 26, nil).current?
+      assert_false Kaminari::Helpers::Paginator::PageProxy.new({ current_page: 13 }, 26, nil).current?
     end
 
     test '#first?' do
       # page == 1
-      assert_true Kaminari::Helpers::Paginator::PageProxy.new({current_page: 26}, 1, nil).first?
+      assert_true Kaminari::Helpers::Paginator::PageProxy.new({ current_page: 26 }, 1, nil).first?
       # page != 1
-      assert_false Kaminari::Helpers::Paginator::PageProxy.new({current_page: 13}, 2, nil).first?
+      assert_false Kaminari::Helpers::Paginator::PageProxy.new({ current_page: 13 }, 2, nil).first?
     end
 
     test '#last?' do
       # current_page == page
-      assert_true Kaminari::Helpers::Paginator::PageProxy.new({total_pages: 39}, 39, nil).last?
+      assert_true Kaminari::Helpers::Paginator::PageProxy.new({ total_pages: 39 }, 39, nil).last?
       # current_page != page
-      assert_false Kaminari::Helpers::Paginator::PageProxy.new({total_pages: 39}, 38, nil).last?
+      assert_false Kaminari::Helpers::Paginator::PageProxy.new({ total_pages: 39 }, 38, nil).last?
     end
 
     test '#next?' do
       # page == current_page + 1
-      assert_true Kaminari::Helpers::Paginator::PageProxy.new({current_page: 52}, 53, nil).next?
+      assert_true Kaminari::Helpers::Paginator::PageProxy.new({ current_page: 52 }, 53, nil).next?
       # page != current_page + 1
-      assert_false Kaminari::Helpers::Paginator::PageProxy.new({current_page: 52}, 77, nil).next?
+      assert_false Kaminari::Helpers::Paginator::PageProxy.new({ current_page: 52 }, 77, nil).next?
     end
 
     test '#prev?' do
       # page == current_page - 1
-      assert_true Kaminari::Helpers::Paginator::PageProxy.new({current_page: 77}, 76, nil).prev?
+      assert_true Kaminari::Helpers::Paginator::PageProxy.new({ current_page: 77 }, 76, nil).prev?
       # page != current_page + 1
-      assert_false Kaminari::Helpers::Paginator::PageProxy.new({current_page: 77}, 80, nil).prev?
+      assert_false Kaminari::Helpers::Paginator::PageProxy.new({ current_page: 77 }, 80, nil).prev?
     end
 
     test '#rel' do
       # page == current_page - 1
-      assert_equal 'prev', Kaminari::Helpers::Paginator::PageProxy.new({current_page: 77}, 76, nil).rel
+      assert_equal 'prev', Kaminari::Helpers::Paginator::PageProxy.new({ current_page: 77 }, 76, nil).rel
       # page == current_page
-      assert_nil Kaminari::Helpers::Paginator::PageProxy.new({current_page: 78}, 78, nil).rel
+      assert_nil Kaminari::Helpers::Paginator::PageProxy.new({ current_page: 78 }, 78, nil).rel
       # page == current_page + 1
-      assert_equal 'next', Kaminari::Helpers::Paginator::PageProxy.new({current_page: 52}, 53, nil).rel
+      assert_equal 'next', Kaminari::Helpers::Paginator::PageProxy.new({ current_page: 52 }, 53, nil).rel
     end
 
     test '#left_outer?' do
       # current_page == left
-      assert_true Kaminari::Helpers::Paginator::PageProxy.new({left: 3}, 3, nil).left_outer?
+      assert_true Kaminari::Helpers::Paginator::PageProxy.new({ left: 3 }, 3, nil).left_outer?
       # current_page == left + 1
-      assert_false Kaminari::Helpers::Paginator::PageProxy.new({left: 3}, 4, nil).left_outer?
+      assert_false Kaminari::Helpers::Paginator::PageProxy.new({ left: 3 }, 4, nil).left_outer?
       # current_page == left + 2
-      assert_false Kaminari::Helpers::Paginator::PageProxy.new({left: 3}, 5, nil).left_outer?
+      assert_false Kaminari::Helpers::Paginator::PageProxy.new({ left: 3 }, 5, nil).left_outer?
     end
 
     test '#right_outer?' do
       # total_pages - page > right
-      assert_false Kaminari::Helpers::Paginator::PageProxy.new({total_pages: 10, right: 3}, 6, nil).right_outer?
+      assert_false Kaminari::Helpers::Paginator::PageProxy.new({ total_pages: 10, right: 3 }, 6, nil).right_outer?
       # total_pages - page == right
-      assert_false Kaminari::Helpers::Paginator::PageProxy.new({total_pages: 10, right: 3}, 7, nil).right_outer?
+      assert_false Kaminari::Helpers::Paginator::PageProxy.new({ total_pages: 10, right: 3 }, 7, nil).right_outer?
       # total_pages - page < right
-      assert_true Kaminari::Helpers::Paginator::PageProxy.new({total_pages: 10, right: 3}, 8, nil).right_outer?
+      assert_true Kaminari::Helpers::Paginator::PageProxy.new({ total_pages: 10, right: 3 }, 8, nil).right_outer?
     end
 
     sub_test_case '#inside_window?' do
       test 'page > current_page' do
         # page - current_page > window
-        assert_false Kaminari::Helpers::Paginator::PageProxy.new({current_page: 4, window: 5}, 10, nil).inside_window?
+        assert_false Kaminari::Helpers::Paginator::PageProxy.new({ current_page: 4, window: 5 }, 10, nil).inside_window?
         # page - current_page == window
-        assert_true Kaminari::Helpers::Paginator::PageProxy.new({current_page: 4, window: 6}, 10, nil).inside_window?
+        assert_true Kaminari::Helpers::Paginator::PageProxy.new({ current_page: 4, window: 6 }, 10, nil).inside_window?
         # page - current_page < window
-        assert_true Kaminari::Helpers::Paginator::PageProxy.new({current_page: 4, window: 7}, 10, nil).inside_window?
+        assert_true Kaminari::Helpers::Paginator::PageProxy.new({ current_page: 4, window: 7 }, 10, nil).inside_window?
       end
 
       test 'current_page > page' do
         # current_page - page > window
-        assert_false Kaminari::Helpers::Paginator::PageProxy.new({current_page: 15, window: 4}, 10, nil).inside_window?
+        assert_false Kaminari::Helpers::Paginator::PageProxy.new({ current_page: 15, window: 4 }, 10, nil).inside_window?
         # current_page - page == window
-        assert_true Kaminari::Helpers::Paginator::PageProxy.new({current_page: 15, window: 5}, 10, nil).inside_window?
+        assert_true Kaminari::Helpers::Paginator::PageProxy.new({ current_page: 15, window: 5 }, 10, nil).inside_window?
         # current_page - page < window
-        assert_true Kaminari::Helpers::Paginator::PageProxy.new({current_page: 15, window: 6}, 10, nil).inside_window?
+        assert_true Kaminari::Helpers::Paginator::PageProxy.new({ current_page: 15, window: 6 }, 10, nil).inside_window?
       end
     end
 
@@ -214,7 +215,7 @@ if defined?(::Rails::Railtie) && defined?(ActionView)
 
     sub_test_case '#single_gap?' do
       setup do
-        @window_options = {left: 1, window: 1, right: 1, total_pages: 9}
+        @window_options = { left: 1, window: 1, right: 1, total_pages: 9 }
       end
 
       def gap_for(page)
@@ -257,11 +258,11 @@ if defined?(::Rails::Railtie) && defined?(ActionView)
 
     test '#out_of_range?' do
       # within range
-      assert_false Kaminari::Helpers::Paginator::PageProxy.new({total_pages: 5}, 4, nil).out_of_range?
+      assert_false Kaminari::Helpers::Paginator::PageProxy.new({ total_pages: 5 }, 4, nil).out_of_range?
       # on last page
-      assert_false Kaminari::Helpers::Paginator::PageProxy.new({total_pages: 5}, 5, nil).out_of_range?
+      assert_false Kaminari::Helpers::Paginator::PageProxy.new({ total_pages: 5 }, 5, nil).out_of_range?
       # out of range
-      assert_true Kaminari::Helpers::Paginator::PageProxy.new({total_pages: 5}, 6, nil).out_of_range?
+      assert_true Kaminari::Helpers::Paginator::PageProxy.new({ total_pages: 5 }, 6, nil).out_of_range?
     end
   end
 end
